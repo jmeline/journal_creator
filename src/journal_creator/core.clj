838248de9f24027d647ea01254bafab3561eb87c
Jacob Meline <jacob.meline@gmail.com>
2017-08-12 14:18:42 -0600

    Changed parameter to recieve a higher-order function

--- src/journal_creator/core.clj
+++ src/journal_creator/core.clj
@@ -1,6 +1,5 @@
 (ns journal-creator.core
   (require [clj-time.core     :as t]
-           [clj-time.local    :as l]
            [clj-time.periodic :as p]
            [clj-time.format   :as f]))
 
@@ -13,32 +12,28 @@
 (defn get-today []
   (t/to-time-zone (t/now) (t/default-time-zone)))
 
-(defn parse-date [date format]
-  (f/unparse (f/formatter-local format) date))
+(defn parse-date-generator [date]
+  (fn [format] (f/unparse (f/formatter-local format) date)))
 
-(defn get-journal-header-str [date]
-  (-> 
-    date
-    #(parse-date % "MMMM dd, yyyy, EEEE") 
-    #(str "= " % " - <SUMMARY GOES HERE> =")))
+(defn get-journal-header-str [date-parser]
+  (-> (date-parser "MMMM dd, yyyy, EEEE") 
+      #(str "= " % " - <SUMMARY GOES HERE> =")))
 
-(defn get-filename-extension-str [date]
-  (-> 
-    date
-    #(parse-date "yyyy-MM-dd" %)
-    #(str % ".wiki")))
+(defn get-filename-extension-str [date-parser]
+  (-> (date-parser "yyyy-MM-dd")
+      #(str % ".wiki")))
 
 (defn save [day] 
-  (let [filename (get-filename-extension-str day)
-        header (get-journal-header-str day)] 
+  (let [date-parser-function (parse-date-generator day)
+        filename (get-filename-extension-str date-parser-function)
+        header (get-journal-header-str date-parser-function)] 
     (spit filename header)))
 
 (defn iterate-times [times]
   (let [current-date (get-today)]
-  (map #(save %) 
-       (time-range current-date
-                   (t/plus current-date (t/days times)) (t/days 1)))))
+    (map #(save %) 
+         (time-range current-date
+                     (t/plus current-date (t/days times)) (t/days 1)))))
 
-(defn -main [& args]
-  (prn "args: " args)
+(defn -main [& args] 
   (iterate-times (read-string (first args)))) 
