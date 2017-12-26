d4ec8686b413b8c53e6f0298e063b83eb09880fb
Jacob Meline <jacob.meline@gmail.com>
2017-12-25 22:24:27 -0700

    Adding support for clojure 1.9.0

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject journal_creator "0.1.0-SNAPSHOT"
+(defproject journal_creator "0.1.0"
   :description "Creates journal headers for vimwiki"
   :url "github.com/jmeline/journal_creator"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-time "0.13.0"]]
-  :main ^:skip-aot journal-creator.core
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [clj-time "0.14.2"]]
+  :main ^:skip-aot journal_creator.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/journal_creator/core.clj
+++ src/journal_creator/core.clj
@@ -1,12 +1,14 @@
-(ns journal-creator.core
-  (require [clj-time.core     :as t]
-           [clj-time.periodic :as p]
-           [clj-time.format   :as f]))
+(ns journal_creator.core
+  (:require [clojure.java.io   :as io]
+            [clj-time.core     :as t]
+            [clj-time.periodic :as p]
+            [clj-time.format   :as f])
+  (:gen-class))
 
 (defn time-range
   [start end step]
   (let [inf-range (p/periodic-seq start step)
-        below-end? (fn [t] (t/within? (t/interval start end) t))]
+        below-end? #(t/within? (t/interval start end) %)]
     (take-while below-end? inf-range)))
 
 (defn get-today []
@@ -16,25 +18,39 @@
   (fn [format] (f/unparse (f/formatter-local format) date)))
 
 (defn get-journal-header [date-parser]
-   (-> "MMMM dd, yyyy, EEEE"
-       date-parser
-       (#(str "= " % " - <SUMMARY GOES HERE> =")))) 
+  (-> "MMMM dd, yyyy, EEEE"
+      date-parser
+      (#(str "= " % " - <SUMMARY GOES HERE> ="))))
 
 (defn get-file-extension [date-parser]
-  (->  "yyyy-MM-dd" 
-       date-parser 
+  (->  "yyyy-MM-dd"
+       date-parser
        (#(str % ".wiki"))))
 
 (defn save [day]
   (let [date-parser-function (parse-date-generator day)
         filename (get-file-extension date-parser-function)
         header (get-journal-header date-parser-function)]
-    (spit filename header)))
+    (when (not (.exists (io/file filename)))
+      (spit filename header))))
 
-(defn iterate-times [times]
-  (let [current-date (get-today)]
-    (doseq [day (time-range current-date (t/plus current-date (t/days times)) (t/days 1))]
-      (save day))))
+(defn iterate-times [date-list]
+  (doseq [day date-list]
+    (save day)))
+
+(defn parse-value [value]
+  (read-string (clojure.string/replace value #"[+-]" "")))
+
+(defn get-date-modify-fn [arg]
+  (let [value (re-find #"[+-]" arg)
+        times (t/days (parse-value arg))
+        current-date (get-today)]
+    (if (= "-" value)
+      #(% (t/minus current-date times) current-date (t/days 1))
+      #(% current-date (t/plus current-date times) (t/days 1)))))
 
 (defn -main [& args]
-  (iterate-times (read-string (first args))))
+  (when (> (count args) 0)
+    (iterate-times
+      ((get-date-modify-fn (first args)) time-range)))
+  (println "Done!"))
